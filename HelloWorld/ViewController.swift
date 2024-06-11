124c22fd368d6971d6d616faa01c17ec11f4d690
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-06-11 14:48:38 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by WEBSYSTEM-MAC41 on 2024/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
